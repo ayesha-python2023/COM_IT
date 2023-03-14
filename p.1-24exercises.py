71daefaaa5b55bee252048de497288f46dd0d806
ayesha-python2023 <ayesha@protonmail.ch>
2023-03-14 17:24:07 -0600

    calculator

--- p.1-24exercises.py
+++ p.1-24exercises.py
@@ -1,22 +1,11 @@
 # 24 - Make_a_calculator.
 
-input_1 = int(input("Please enter the first number"))
-input_2 = int(input("Please enter the second number"))
+input_1 = input("Please enter the first number: ")
+operator = input("Please insert the operator: ")
+input_2 = input("Please enter the second number: ")
 
-addition = int(input_1 + input_2)
+result = eval(input_1 + operator + input_2)
 
-subtraction = int(input_1 - input_2)
-
-multiplication = int(input_1 * input_2)
-
-division = float(input_1 // input_2)
-
-exponential = int(input_1 ** input_2)
-
-print(f"{input_1} + {input_2} is equals to {addition}")
-print(f"{input_1} - {input_2} is equals to {subtraction}")
-print(f"{input_1} * {input_2} is equals to {multiplication}")
-print(f"{input_1} + {input_2} is equals to {division}")
-print(f"{input_1} ** {input_2} is equals to {exponential}")
+print(f"{input_1} {operator} {input_2} is equals to {result}")
 
 
