9e9e45bffcbd19652d61a8d9e793f0a381cfd280
ayesha-python2023 <ayesha@protonmail.ch>
2023-03-15 10:58:59 -0600

    Calculator

--- p.1-24exercises.py
+++ p.1-24exercises.py
@@ -1,11 +1,9 @@
 # 24 - Make_a_calculator.
 
 input_1 = input("Please enter the first number: ")
-operator = input("Please insert the operator: ")
+operator = input("Please enter an operator")
 input_2 = input("Please enter the second number: ")
 
 result = eval(input_1 + operator + input_2)
 
-print(f"{input_1} {operator} {input_2} is equals to {result}")
-
-
+print(f"{input_1} {operator} {input_2} is equal to {result}")
