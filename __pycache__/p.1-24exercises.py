0b4c0d08b6989eaaecd93493917e8edcde46762d
Ayesha_Zaheer <125105395+ayesha-python2023@users.noreply.github.com>
2023-03-23 12:31:14 -0600

    Delete __pycache__ directory

--- __pycache__/p.1-24exercises.py
+++
@@ -1,9 +0,0 @@
-# 24 - Make_a_calculator.
-
-input_1 = input("Please enter the first number: ")
-operator = input("Please enter an operator")
-input_2 = input("Please enter the second number: ")
-
-result = eval(input_1 + operator + input_2)
-
-print(f"{input_1} {operator} {input_2} is equal to {result}")
