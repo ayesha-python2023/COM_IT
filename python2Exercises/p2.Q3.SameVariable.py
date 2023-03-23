10715e4633be2ddd08efc42f079b4a645cedc9c9
ayesha-python2023 <ayesha@protonmail.ch>
2023-03-23 16:27:04 -0600

    same variable

---
+++ python2Exercises/p2.Q3.SameVariable.py
@@ -0,0 +1,6 @@
+# WRITE AN ALGORITHM AND SUBALGORITHM. WRITE TWO VARIABLES WITH THE SAME NAME SO THAT THE COMPILER DOES NOT SHOW AN ERROR.
+
+"""
+    _This is the same exercise that is under the name "p.1-29VariableWithSameValue.py" 
+    in the folder "python1Exercises"_
+"""
